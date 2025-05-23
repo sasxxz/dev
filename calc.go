7575e51a9a5ebe07b980b6657b9d1b1368be215b
Your Name <you@example.com>
2025-05-23 16:02:23 +0800

    calc ex

---
+++ calc.go
@@ -0,0 +1,17 @@
+package calc
+
+func Add(A, B int) int {
+	return A + B
+}
+
+func Sub(A, B int) int {
+	return A - B
+}
+
+func Mul(A, B int) int {
+	return A * B
+}
+
+func Div(A, B int) int {
+	return A / B
+}
